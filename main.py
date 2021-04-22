82349f0ff6051dfb422d6c04bb8f1d44bfe4bb44
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2021-04-22 12:09:23 +1200

    removed stray print()

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     url = "https://deaths.dompost.co.nz/obituaries/dominion-post-nz/"
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     response = requests.get(url, headers=headers)
-    print(response)
     soup = BeautifulSoup(response.text, "html.parser")
     for li in soup.findAll('ul', {'class': "recentObitsList"}):
         name = li.text.replace("(Death Notice)", "")
